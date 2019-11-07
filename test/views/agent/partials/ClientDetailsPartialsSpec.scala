e453c7806c3cb7b1aaa7e12475fc2c23c15ea541
JoshuaGomersallHMRC <53816372+JoshuaGomersallHMRC@users.noreply.github.com>
2019-11-07 16:03:26 +0000

    Updated test to use new object and updated messages to use ’

--- test/views/agent/partials/ClientDetailsPartialsSpec.scala
+++ test/views/agent/partials/ClientDetailsPartialsSpec.scala
@@ -21,6 +21,7 @@ import org.jsoup.nodes.Document
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
 import views.ViewBaseSpec
+import assets.messages.partials.{ClientDetailsPartialMessages => Messages}
 
 class ClientDetailsPartialsSpec extends ViewBaseSpec {
 
@@ -31,15 +32,15 @@ class ClientDetailsPartialsSpec extends ViewBaseSpec {
     lazy implicit val document: Document = Jsoup.parse(view.body)
 
     s"have the correct card heading" in {
-      elementText(".heading-medium") shouldBe "Client details"
+      elementText(".heading-medium") shouldBe Messages.heading
     }
 
     "display the correct line 1" in {
-      elementText("#card-info") shouldBe "Change client business, contact and VAT details."
+      elementText("#card-info") shouldBe Messages.paragraphOne
     }
 
     "display the correct line 2" in {
-      elementText("#card-link") shouldBe "View your client's VAT details"
+      elementText("#card-link") shouldBe Messages.link
     }
 
     "display the correct line 2 with the correct link" in {
--- test/views/agent/partials/VatCertificatePartialsSpec.scala
+++ test/views/agent/partials/VatCertificatePartialsSpec.scala
@@ -21,6 +21,7 @@ import org.jsoup.nodes.Document
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
 import views.ViewBaseSpec
+import assets.messages.partials.{VatCertificatePartialMessages => Messages}
 
 class VatCertificatePartialsSpec extends ViewBaseSpec {
 
@@ -31,15 +32,15 @@ class VatCertificatePartialsSpec extends ViewBaseSpec {
     lazy implicit val document: Document = Jsoup.parse(view.body)
 
     s"have the correct card heading" in {
-      elementText(".heading-medium") shouldBe "VAT certificate"
+      elementText(".heading-medium") shouldBe Messages.heading
     }
 
     "display the correct line 1" in {
-      elementText("#card-info") shouldBe "View and print your client's VAT certificate."
+      elementText("#card-info") shouldBe Messages.paragraphOne
     }
 
     "display the correct line 2" in {
-      elementText("#card-link") shouldBe "View VAT certificate"
+      elementText("#card-link") shouldBe Messages.link
     }
 
     "display the correct line 2 with the correct link" in {
