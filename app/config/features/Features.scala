fdd5c1b08226bec27379071505ebb6a8f2f7889f
Sheila Tran <63787618+SheilaTran@users.noreply.github.com>
2021-06-03 10:42:08 +0100

    BTAT-9290 add direct debit feature switch

--- app/config/features/Features.scala
+++ app/config/features/Features.scala
@@ -24,4 +24,5 @@ class Features @Inject()(config: Configuration) {
 
   val emailVerificationEnabled = new Feature(ConfigKeys.emailVerificationFeature, config)
   val useStaticDateFeature = new Feature(ConfigKeys.useStaticDateFeature, config)
+  val directDebitInterruptFeature = new Feature(ConfigKeys.directDebitInterruptFeature, config)
 }
