75e7af8d495e5bdeea738b178947d901118e7415
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2021-11-05 15:01:22 +0000

    [BTAT-9823] - Updated URL

--- it/stubs/PenaltiesStub.scala
+++ it/stubs/PenaltiesStub.scala
@@ -7,7 +7,7 @@ import play.api.libs.json.JsValue
 
 object PenaltiesStub extends WireMockMethods {
 
-  def penaltiesServiceUrl(vrn: String) = s"/vat/penalties/summary/$vrn"
+  def penaltiesServiceUrl(vrn: String) = s"/penalties/vat/penalties/summary/$vrn"
 
   def stubPenaltiesSummary(status: Int, response: JsValue, vrn: String): StubMapping =
     when(method = GET, uri = penaltiesServiceUrl(vrn))
