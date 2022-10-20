b4ad905c35a4ed9695f5133df9aecea06ee45028
william-hatzar <22661741+william-hatzar@users.noreply.github.com>
2022-10-20 10:35:06 +0100

    Updated test that was failing due to link text change

--- test/assets/messages/partials/NextPaymentPartialMessages.scala
+++ test/assets/messages/partials/NextPaymentPartialMessages.scala
@@ -21,7 +21,7 @@ object NextPaymentPartialMessages {
   val headingPayments: String = "Payments due"
   val noPayments: String = "No payments due right now"
   val payments: String = "You have 10 payments due"
-  val linkText: String = "Check what you owe"
+  val linkText: String = "Check what your client owes"
   val date: String = "1 January 2018"
   val overdue: String = "overdue"
   val ddSetUp: String = "Direct Debit is set up"
