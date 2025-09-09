9071f5afb4bb5cbeb53dc77f10dff2d6d7adec4c
Michele Satta <3033136+msatta@users.noreply.github.com>
2025-09-09 11:19:54 +0100

    [DL-17290] - Update to use the new agnostic penalties endpoints

--- it/stubs/PenaltiesStub.scala
+++ it/stubs/PenaltiesStub.scala
@@ -22,7 +22,7 @@ import play.api.libs.json.JsValue
 
 object PenaltiesStub extends WireMockMethods {
 
-  def penaltiesServiceUrl(vrn: String) = s"/penalties/vat/penalties/summary/$vrn"
+  def penaltiesServiceUrl(vrn: String) = s"/penalties/VATC/summary/VRN/$vrn"
 
   def stubPenaltiesSummary(status: Int, response: JsValue, vrn: String): StubMapping =
     when(method = GET, uri = penaltiesServiceUrl(vrn))
